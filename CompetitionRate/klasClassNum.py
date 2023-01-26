71f1fbedaf49c9f7b37e1d8785573c99ab45f457
오민성 <82754127+nnin5e0ng@users.noreply.github.com>
2023-01-26 16:20:03 +0900

    전과목 테스트

--- CompetitionRate/klasClassNum.py
+++ CompetitionRate/klasClassNum.py
@@ -29,37 +29,45 @@ def lookUp():
     driver.find_element_by_xpath('//*[@id="selecthakgi"]/option[3]').click()
     
     df = pd.read_csv("2022년2학기.csv")
-    # 과목과 담당교수 입력
-    lecture = driver.find_element_by_xpath('//*[@id="appModule"]/div[2]/div[2]/table[1]/tbody/tr[2]/td[1]/input')
-    lecture.send_keys(df.과목명[0])
-    lecture = driver.find_element_by_xpath('//*[@id="appModule"]/div[2]/div[2]/table[1]/tbody/tr[2]/td[2]/input')
-    lecture.send_keys(df.담당교수[0])
     
-    time.sleep(1.5)
+    for i in range(df.shape[0]):
+        
+        # 과목과 담당교수 입력
+        lecture = driver.find_element_by_xpath('//*[@id="appModule"]/div[2]/div[2]/table[1]/tbody/tr[2]/td[1]/input')
+        lecture.clear()
+        lecture.send_keys(df.과목명[i])
+        
+        professor = driver.find_element_by_xpath('//*[@id="appModule"]/div[2]/div[2]/table[1]/tbody/tr[2]/td[2]/input')
+        professor.clear()
+        professor.send_keys(df.담당교수[i])
+        
+        time.sleep(1.5)
+        
+        # 인증코드
+        code = driver.find_element_by_xpath('//*[@id="appModule"]/div[2]/div[2]/table[1]/tbody/tr[5]/td/span').text
+        driver.find_element_by_xpath('//*[@id="appModule"]/div[2]/div[2]/table[1]/tbody/tr[5]/td/input').send_keys(code)
+        
+        # 강의정보 조회
+        driver.find_element_by_xpath('//*[@id="appModule"]/div[2]/div[2]/div/button').send_keys(Keys.RETURN)
+        time.sleep(1.5)
+        driver.find_element_by_xpath('//*[@id="appModule"]/div[2]/div[2]/table[2]/tbody/tr/td[2]').click()
+        time.sleep(1)
+        
+        # 수강인원 조회
+        driver.find_element_by_xpath('//*[@id="appModule"]/div[2]/div[2]/table[1]/tbody/tr[4]/td[2]/button').click()
+        
+        driver.switch_to.window(driver.window_handles[1]) 
     
-    # 인증코드
-    code = driver.find_element_by_xpath('//*[@id="appModule"]/div[2]/div[2]/table[1]/tbody/tr[5]/td/span').text
-    driver.find_element_by_xpath('//*[@id="appModule"]/div[2]/div[2]/table[1]/tbody/tr[5]/td/input').send_keys(code)
-    
-    # 강의정보 조회
-    driver.find_element_by_xpath('//*[@id="appModule"]/div[2]/div[2]/div/button').send_keys(Keys.RETURN)
-    time.sleep(1.5)
-    driver.find_element_by_xpath('//*[@id="appModule"]/div[2]/div[2]/table[2]/tbody/tr/td[2]').click()
-    time.sleep(1)
-    
-    # 수강인원 조회
-    driver.find_element_by_xpath('//*[@id="appModule"]/div[2]/div[2]/table[1]/tbody/tr[4]/td[2]/button').click()
-    
-    driver.switch_to.window(driver.window_handles[1])
-
-    code = driver.find_element_by_xpath('//*[@id="appModule"]/div/div[2]/table/tbody/tr/td/span').text
-    driver.find_element_by_xpath('//*[@id="appModule"]/div/div[2]/table/tbody/tr/td/input').send_keys(code)
-    time.sleep(1)
-    driver.find_element_by_xpath('//*[@id="appModule"]/div/div[2]/table/tbody/tr/td/button[2]').click()
-    time.sleep(1)
-    people = driver.find_element_by_xpath('//*[@id="appModule"]/div/div[2]/div/b[2]').text
-    
-    return people
+        code = driver.find_element_by_xpath('//*[@id="appModule"]/div/div[2]/table/tbody/tr/td/span').text
+        driver.find_element_by_xpath('//*[@id="appModule"]/div/div[2]/table/tbody/tr/td/input').send_keys(code)
+        time.sleep(1)
+        driver.find_element_by_xpath('//*[@id="appModule"]/div/div[2]/table/tbody/tr/td/button[2]').click()
+        time.sleep(1)
+        people = driver.find_element_by_xpath('//*[@id="appModule"]/div/div[2]/div/b[2]').text
+        
+        print(people)
+        driver.switch_to.window(driver.window_handles[0])
+        driver.find_element_by_xpath('//*[@id="appModule"]/div[2]/div[1]/span/button').click()
 
 
 if __name__ == "__main__":
@@ -67,5 +75,6 @@ if __name__ == "__main__":
     chromedriver ='C:\\Users\\minseong\\Desktop\\MyStudy\\project_광운대학교_강의추천\\chromedriver_win32\\chromedriver.exe'
     driver = webdriver.Chrome(chromedriver)
 
-    login("학번", "**^^**")
-    print(lookUp())
+    login("학번", "비번")
+    lookUp()
+    
